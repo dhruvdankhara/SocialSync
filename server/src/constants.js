708bc0d195f45fb3127e5d54b7105bff02d2d26d
dhruvdankhara <dhruvdankhara09@gmail.com>
2024-06-21 14:19:10 +0530

    Update user model, Add bcrypt and jsonwebtoken dependencies

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,1 +1,16 @@
 export const DB_NAME = "SocialSync";
+
+export const UserRolesEnum = {
+  USER: "USER",
+  ADMIN: "ADMIN",
+};
+
+export const AvailableUserRoles = Object.values(UserRolesEnum);
+
+export const UserGenderEnum = {
+  MALE: "MALE",
+  FEMALE: "FEMALE",
+  OTHER: "OTHER",
+};
+
+export const AvailableUserGender = Object.values(UserGenderEnum);
--- server/src/model/user.model.js
+++ server/src/model/user.model.js
@@ -1,20 +1,27 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {
+  AvailableUserGender,
+  AvailableUserRoles,
+  UserGenderEnum,
+  UserRolesEnum,
+} from "../constants";
 
 const userSchema = new mongoose.Schema(
   {
-    fullName: {
+    name: {
       type: String,
       required: true,
       trim: true,
-      index: true,
     },
     username: {
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
       trim: true,
       index: true,
+      lowercase: true,
     },
     email: {
       type: String,
@@ -29,11 +36,18 @@ const userSchema = new mongoose.Schema(
     },
     gender: {
       type: String,
-      default: "Male",
-      enum: ["Male", "Female", "Other"],
+      default: UserGenderEnum.MALE,
+      enum: AvailableUserGender,
     },
     avatar: {
       type: String, // cloudnary URL
+      default: "",
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: AvailableUserRoles,
+      default: UserRolesEnum.USER,
       required: true,
     },
     refreshToken: {
@@ -42,5 +56,29 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      role: this.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
